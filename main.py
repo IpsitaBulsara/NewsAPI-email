6326a73d3fdee6a8e3a9a95abe1f582c47425a69
Ipsita Bulsara <bulsaraipsita@gmail.com>
2025-03-11 12:24:41 +0530

    sent email of all the news with specific language and added limit in number of news sent.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
+from send_email import send_email
 
-api_key = "abc"
-url = "xyz"
+topic="tesla"
+api_key = "a00"
+url = "https://newsapi.org/v2/everything?" \
+f"q={topic}" \
+"&from=2025-02-11&" \
+"sortBy=publishedAt&" \
+"apiKey=a00&language=en"
 
 #Make request
 request = requests.get(url)
@@ -10,5 +16,14 @@ request = requests.get(url)
 content = request.json()
 
 #Access the article titles and description
-for article in content["articles"]:
-    print(article["title"])
+body=""
+for article in content["articles"][:20]:
+
+    if article["title"]is not None:
+        body = "Subject: Today's news" \
+               + "\n" + body + str(article["title"]) \
+               + "\n" + str(article["description"]) + \
+               "\n" +str(article["url"])+  2*"\n"
+
+body = body.encode("utf-8")
+send_email(body)
